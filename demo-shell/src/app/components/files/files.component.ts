7ab79608dc4bb32659fb62d3d35a1172f85c88e6
VitoAlbano <vito.albano.123@gmail.com>
2018-07-12 17:07:14 +0100

    [ADF-3336] fixed error on files component

--- demo-shell/src/app/components/files/files.component.ts
+++ demo-shell/src/app/components/files/files.component.ts
@@ -177,6 +177,7 @@ export class FilesComponent implements OnInit, OnChanges, OnDestroy {
     warnOnMultipleUploads = false;
     thumbnails = false;
     enableCustomPermissionMessage = false;
+    enableMediumTimeFormat = false;
 
     private onCreateFolder: Subscription;
     private onEditFolder: Subscription;
